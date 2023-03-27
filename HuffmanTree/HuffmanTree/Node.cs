4911f76dcee0caebbd9d0b63426f6d6ff1d1c011
GodoFFLuckk <103437149+GodoFFLuckk@users.noreply.github.com>
2023-03-27 15:43:10 +0200

    Add files via upload

---
+++ HuffmanTree/HuffmanTree/Node.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuffmanTree
+{
+    internal class Node : IComparable<Node>
+    {
+        private int weight;
+        private Node leftSon;
+        private Node rightSon;
+        private int codeNumber;
+        private int era;
+        public Node(Node leftSon, Node rightSon, int weight, int codeNumber, int era)
+        {
+            this.leftSon = leftSon;
+            this.rightSon = rightSon;
+            this.weight = weight;
+            this.codeNumber = codeNumber;
+            this.era = era;
+        }
+        public int getWeight()
+        {
+            return weight;
+        }
+        public Node getLeftSon()
+        {
+            return leftSon;
+        }
+        public Node getRightSon()
+        {
+            return rightSon;
+        }
+        public int getEra()
+        {
+            return era;
+        }
+        public int getCodeNumber()
+        {
+            return codeNumber;
+        }
+        public int CompareTo(Node node)
+        {
+            if (this.weight > node.weight)
+            {
+                return 1;
+            }
+            else if (this.weight < node.weight)
+            {
+                return -1;
+            }
+            else
+            {
+                if (codeNumber < 256)
+                {
+                    if (this.codeNumber > node.codeNumber)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return -1;
+                    }
+                }
+                else
+                {
+                    if (this.era > node.era)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return -1;
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ HuffmanTree/HuffmanTree/Tree.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuffmanTree
+{
+    internal class Tree
+    {
+        private List<Node> nodes = new List<Node>();
+        private Node root = null;
+        private int miniTreesCount = 0;
+        private int nodesCount = 0;
+        public void append(Node node)
+        {
+            miniTreesCount++;
+            nodes.Add(node);
+        }
+        int epochs = 0;
+        public void TransformToHuffman()
+        {
+            nodesCount = miniTreesCount * 2 - 1;
+            nodes.Sort();
+            while (miniTreesCount > 1)
+            {
+                epochs++;
+                Node newNode = new Node(nodes[0], nodes[1], nodes[0].getWeight() + nodes[1].getWeight(), 10000000, epochs);
+                nodes.Add(newNode);
+                nodes.RemoveAt(0);
+                nodes.RemoveAt(0);
+                nodes.Sort();
+                miniTreesCount--;
+            }
+            root = nodes[0];
+        }
+        public Node GetRoot()
+        {
+            return root;
+        }
+        public void PrintTreeInPreorder()
+        {
+            int nodesCountForPrint = this.nodesCount;
+            if (root != null)
+            {
+                PrintTreeInPreorder(root, nodesCountForPrint);
+            }
+        }
+        public void PrintTreeInPreorder(Node curNode, int nodesCountForPrint)
+        {
+            nodesCountForPrint--;
+            if (curNode.getEra() > 0)
+            {
+                Console.Write(curNode.getWeight());
+                if (nodesCount != 0)
+                {
+                    Console.Write(' ');
+                }
+            }
+            else
+            {
+                Console.Write('*');
+                Console.Write(curNode.getCodeNumber());
+                Console.Write(':');
+                Console.Write(curNode.getWeight());
+                if (nodesCount != 0)
+                {
+                    Console.Write(' ');
+                }
+            }
+            if (curNode.getLeftSon() != null)
+            {
+                PrintTreeInPreorder(curNode.getLeftSon(), nodesCountForPrint);
+            }
+            if (curNode.getRightSon() != null)
+            {
+                PrintTreeInPreorder(curNode.getRightSon(), nodesCountForPrint);
+            }
+        }
+    }
+}
